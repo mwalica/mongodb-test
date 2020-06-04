e30cdace43e168840daed66bca594fddffcdac95
Marek Walica <mwalica@gmai.com>
2020-06-04 11:21:26 +0200

    init commit

---
+++ mongo.js
@@ -0,0 +1,42 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const url = "mongodb://localhost:27017/academind";
+
+const createProduct = async (req, res, next) => {
+  const newProduct = {
+    name: req.body.name,
+    price: req.body.price,
+  };
+
+  const client = new MongoClient(url);
+  try {
+      await client.connect();
+      const db = client.db();
+      const result = db.collection('products').insertOne(newProduct);
+  } catch (error) {
+    return res.json({ message: "Could not store data." });
+  };
+  client.close();
+  res.json(newProduct);
+};
+
+
+
+const getProducts = async (req, res, next) => {
+  const client = new MongoClient(url);
+
+  let products;
+  try {
+    await client.connect();
+    const db = client.db();
+    products = await db.collection('products').find().toArray();
+  } catch (error) {
+    return res.json({message: 'Could not retrieve products.'});
+  };
+
+  client.close(0);
+  res.json(products);
+};
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
